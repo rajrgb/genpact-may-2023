1d1db949363a6ce916b69deb1d2f68b2ce88f760
Raj Kumar Tiwari <kumarraj7198@gmail.com>
2023-05-22 15:57:23 +0530

    update

--- CoreJava/src/com/org/day1/SecondLargest.java
+++ CoreJava/src/com/org/day1/SecondLargest.java
@@ -1,12 +1,26 @@
 package com.org.day1;
 
 public class SecondLargest {
-public int solve(float[] arr) {
+public int solve(float[] arr, int n) {
 	int i;
 	int pos=-1;
 	int p=0;
 	float max=arr[pos];
-	for()
-	return pos;
+	for(i=0;i<n;i++) {
+		if(arr[i]>max) {
+
+			max=arr[i];
+			pos=i;
+		}
+	}
+	p=pos+1;
+	float secMax=arr[(pos+1)%n];
+	for(i=0;i<n;i++) {
+		if(arr[i]>secMax&&i!=pos) {
+			p=i;
+			secMax=arr[i];
+		}
+	}
+	return p;
 }
 }
